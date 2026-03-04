446544e7a3c0a8e5db81cf7a9415b2148406fb02
Denis-Tyvonyuk <denusa321@gmail.com>
2026-03-04 19:51:23 +0200

    realtime message setup

---
+++ src/lib/realtime.ts
@@ -0,0 +1,27 @@
+import { InferRealtimeEvents, Realtime } from "@upstash/realtime";
+import { redis } from "../lib/redis";
+import z from "zod";
+
+const message = z.object({
+  id: z.string(),
+  sender: z.string(),
+  text: z.string(),
+  timestamp: z.string(),
+  roomId: z.string(),
+  token: z.string().optional(),
+});
+
+const schema = {
+  chat: {
+    message,
+    destroy: z.object({
+      isDestroyed: z.literal(true),
+    }),
+  },
+};
+
+export const realtime = new Realtime({ schema, redis });
+
+export type RealTimeEvents = InferRealtimeEvents<typeof realtime>;
+
+export type Message = z.infer<typeof message>;
